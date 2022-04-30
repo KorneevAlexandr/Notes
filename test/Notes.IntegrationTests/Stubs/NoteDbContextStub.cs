b140c230ac9e09b5fbcec055a832ccff5602db42
KorneevAlexandr <sansay_sa@mail.ru>
2022-04-30 20:59:46 +0300

    Integrated Security=False

--- test/Notes.IntegrationTests/Stubs/NoteDbContextStub.cs
+++ test/Notes.IntegrationTests/Stubs/NoteDbContextStub.cs
@@ -5,7 +5,7 @@ namespace Notes.IntegrationTests.Stubs
 {
     public class NoteDbContextStub : DbContext
     {
-        private readonly string _connectionString = "Data Source=localhost; Initial Catalog = NotesGit; Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+        private readonly string _connectionString = "Data Source=localhost; Initial Catalog = NotesGit; Integrated Security=False;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
 
         public NoteDbContextStub()
         { 
