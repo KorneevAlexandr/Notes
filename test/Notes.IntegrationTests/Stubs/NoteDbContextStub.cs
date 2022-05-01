c0c29f61e1fd7aabb8e05f4375e49ef785ce8ed1
KorneevAlexandr <sansay_sa@mail.ru>
2022-05-01 14:49:14 +0300

    u

--- test/Notes.IntegrationTests/Stubs/NoteDbContextStub.cs
+++ test/Notes.IntegrationTests/Stubs/NoteDbContextStub.cs
@@ -5,7 +5,7 @@ namespace Notes.IntegrationTests.Stubs
 {
     public class NoteDbContextStub : DbContext
     {
-        private readonly string _connectionString = "Data Source=localhost; Initial Catalog = NotesGit; Integrated Security=False; User ID = sa; Password=W~08irasa";
+        private readonly string _connectionString = "server=localhost;Database=NotesGit;Integrated Security=False;User=sa;Password=W~08irasa";
 
         public NoteDbContextStub()
         { 
